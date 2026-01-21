eb963f23c8f5b67bdcb9defe7a26b0592b0990f6
aarush2557 <aarush2557@gmail.com>
2026-01-21 10:55:04 +0530

    Day4

---
+++ JS-Day-5/script.js
@@ -0,0 +1,14 @@
+//string method
+// let src = "Java script is fun!";
+// let sliced = src.slice(0,10);
+// cosole.log(sliced);
+
+// let str= "Javascript is amazing!";
+// let sub = str.slice(-2,2);
+// console.log(sub); //negative index supports this but not slice
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+const indexOfFirst = paragraph.indexOf("dog");
+console.log(indexOfFirst);
+let str = "OpenAI is innovative!";
+let sub = str.substring(8, 2); // sub is "is"
