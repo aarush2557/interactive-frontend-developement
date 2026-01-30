a62f400335fff5522ac608177f9cbbaafebf6fb6
aarush2557 <aarush2557@gmail.com>
2026-01-30 15:52:13 +0530

    dom

--- JS-Day-7/script.js
+++ JS-Day-7/script.js
@@ -1,22 +1,43 @@
 
-let user={
-    fullname:"AARUSH TYAGI",
-    address:{
-        city:"sonipat",
-        state:"haryana",
-    },
-    mobileno:"9789897878",
-    favcolor:["pink","red","black"],
-    demo:function(){
-        return "demo function"
-    }
-}
+// let user={
+//     fullname:"AARUSH TYAGI",
+//     address:{
+//         city:"sonipat",
+//         state:"haryana",
+//     },
+//     mobileno:"9789897878",
+//     favcolor:["pink","red","black"],
+//     demo:function(){
+//         return "demo function"
+//     }
+// }
 
-console.log(user.fullname,user.address.city,user.favcolor[0],user.mobileno,user.demo());
+// console.log(user.fullname,user.address.city,user.favcolor[0],user.mobileno,user.demo());
 
-console.log("My name is "+user.fullname+".My fav color is "+user.favcolor[2] );
+// console.log("My name is "+user.fullname+".My fav color is "+user.favcolor[2] );
 
-//string literal
-console.log(`my name is ${user.fullname}.my fav color is ${user.favcolor[2]}`);
+// //string literal
+// console.log(`my name is ${user.fullname}.my fav color is ${user.favcolor[2]}`);
 
-// object methods
+// // object methods
+
+// //Object.keys(variable name)
+
+// console.log(Object.keys(user));
+
+// console.log(object.values(user)):
+ 
+// console.log(object.entries(user))
+
+const car ={
+    make: "mahindra",
+    modle: "thar"
+};
+//
+//object.freeze(car);//we cant add a new key and value pair and we cant change or update the existing value
+
+object.seal(car) // we cant add a new key and value pair but we can change and update the existing value
+
+car.modle="xuv700"
+car.color="zblack";
+console.log(car);
---
+++ JS-Day-8/script.js
@@ -0,0 +1,15 @@
+// const heading = document.getElementById('myHeading');
+// console.log(heading);
+
+// heading.style.color="orange";
+// heading.style.backgroundColor="blue";
+// console.log(heading.textContent);
+// heading.textContent="hello India!";
+
+
+
+const subHeading = document.getElementsByClassName("sub-heading");
+console.log(subHeading);    
+for(let i=0;i<subHeading.length; i++){
+     subHeading[i].style.color = "tomato";
+}
